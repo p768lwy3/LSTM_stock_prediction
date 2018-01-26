7a905a9d8d2547558bea5cfb9fd6034628fa6d84
LI, Wai Yin <p768lwy3@gmail.com>
2018-01-26 16:40:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # import
 import argparse, numpy as np,  matplotlib.pyplot as plt
 import evaluate, run, train, tools
-from datetime import datetime
+from datetime import datetime, strptime
 np.random.seed(12345)
 
 # define console common,
 ap = argparse.ArgumentParser()
 ap.add_argument('--stock_no', type=int, default=1, help='stock Number on HKEX, e.g. for HK0001, input is 1.')
+ap.add_argument('--start_date', type=int, default=20170101, help='start date of learning, format: YYYYMMDD.')
+ap.add_argument('--end_date', type=int, default=20171231, help='end date of learning, format: YYYYMMDD.')
 ap.add_argument('--x_windows', type=int, default=30, help='sliding Windows for input.')
 ap.add_argument('--y_windows', type=int, default=3, help='sliding Windows for output.')
 ap.add_argument('--split_ratio', type=float, default=0.8, help='cross Validation Ratio for Learning.')
@@ -21,6 +23,8 @@ ap.add_argument('--mode', default='train', help=mode_help)
 args = vars(ap.parse_args())
 
 stock_no = args['stock_no'] # define a fn to convert to strings,
+start = args['start_date']
+end = args['end_date']
 x_window_size = args['x_windows']
 y_window_size = args['y_windows']
 split_ratio = args['split_ratio']
@@ -32,8 +36,8 @@ MODE = args['mode']
 
 # this should be added to input
 stock_no = tools.convert_int_to_string(stock_no)
-start = datetime(2010, 1, 1)
-end = datetime(2017, 12, 31)
+start = datetime.strptime(str(start), '%Y%d%m')
+end = datetime.strptime(str(end), '%Y%d%m')
 
 if __name__ == '__main__':
   if MODE == 'train':
@@ -47,4 +51,3 @@ if __name__ == '__main__':
   elif MODE == 'run':
     run.run(stock_no, start, end, x_window_size, y_window_size, 
            split_ratio, layer_num, hidden_dim)
-
